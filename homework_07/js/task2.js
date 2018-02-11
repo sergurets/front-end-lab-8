6b21fc4930530083c4cf38148dec606ab86db3b0
sergurets <sergurets@gmail.com>
2018-02-12 00:09:39 +0200

    Edit prompt, add curprize

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,45 +1,46 @@
-var number, userNumb, prize, arr, max, status;
+var number, userNumb, prize, arr, max, status, maxprize, curPrize;
 prize = 0;
-arr = [10, 5, 2];
+maxprize = 10;
+arr = [Math.floor(maxprize), Math.floor(maxprize / 2), Math.floor(maxprize / 4)];
 max = 5;
 var game = confirm('Do you want to play a game');
-if(game){ 
-
-while (game) {
-  status="";	
-console.log(status);  
-  number = Math.floor(Math.random() * (max + 1));
-  console.log(number);
-  for (var i = 0; i < 3; i++) {
-    userNumb = prompt("Enter a number from 0 to " + max + "\nAttempts left: " + i + "\nTotal prize: " + prize + "$" + "\nPossible prize on current attempt: " + arr[i] + "$","");
-	console.log(userNumb);
-    if (userNumb === null) {
-      break;
+if (game) {
+  while (game) {
+    status = "";
+    number = Math.floor(Math.random() * (max + 1));
+    console.log(number);
+    for (var i = 0; i < 3; i++) {
+      userNumb = prompt("Enter a number from 0 to " + max + "\nAttempts left: " + i + "\nTotal prize: " + prize + "$" + "\nPossible prize on current attempt: " + arr[i] + "$", "");
+      if (userNumb === null) {
+        break;
+      }
+      if (userNumb.length == 0) {
+        status = "";
+      } else if (number == userNumb) {
+        prize = prize + arr[i];
+		curPrize = arr[i];
+        maxprize = 3 * maxprize;
+        arr = [Math.floor(maxprize), Math.floor(maxprize / 2), Math.floor(maxprize / 4)];
+        max = max * 2;
+        status = 'win';
+        break;
+      } else {
+        status = "";
+      }
     }
-    console.log(userNumb);
-	/*userNumb = Number(userNumb);*/
-    if (number == userNumb) {
-      prize = prize + arr[i];
-      /*console.log("You win " + prize + "$");*/
-      multiple(arr);
-      max = max * 2;
-	  status='win';
-      console.log(arr, status);
-      break;
+    if (status) {
+      console.log("You win " + curPrize + "$");
+      game = confirm("Do you want to play again?");
+	  if(!game) {console.log("Thank you for a game. Your prize is " + prize+ "$");}	  
+    } else {
+      prize = 0;
+      maxprize = 10;
+      arr = [Math.floor(maxprize), Math.floor(maxprize / 2), Math.floor(maxprize / 4)];
+      max = 5;
+      console.log("Thank you for a game. Your prize is " + prize+ "$");
+      game = confirm("Do you want to play again?");
     }
-	else {status="";}
   }
-  if(status) {console.log("You win " + prize + "$"); 
-	  game = confirm("Do you want to play again?");}
-  else {console.log("Thank you for a game. Your prize is " + prize);
-	  game = confirm("Do you want to play again?");}
-  
+} else {
+  console.log("You did not become a millionaire")
 }
-
-function multiple(array) {
-  for (var i = 0; i < array.length; i++) {
-    array[i] = array[i] * 3;
-  }
-}
-}
-else {console.log("You did not become a millionaire")}
