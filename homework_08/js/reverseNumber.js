83578313315c01a3ba03f674d8efa6c6f2f0b5a3
Sergiy Smituh <sergurets@gmail.com>
2018-03-13 21:02:46 +0200

    Delete reverseNumber.js

--- homework_08/js/reverseNumber.js
+++
@@ -1,9 +0,0 @@
-function reverseNumber(numb) {
-  var str = String(numb);
-  str = str.split('');
-  if (str[0] === "-") {
-    str[0] = '';
-    str[str.length] = "-";
-  }
-  return (Number(str.reverse().join('')));
-}
