e1cadf511d60838928c9629ba4842d25ef38242c
Sergiy Smituh <sergurets@gmail.com>
2018-03-13 21:02:01 +0200

    Delete getClosestToZero.js

--- homework_08/js/getClosestToZero.js
+++
@@ -1,9 +0,0 @@
-function getClosestToZero() {
-  var min = Infinity;
-  for (var i = 0; i < arguments.length; i++) {
-    if (Math.abs(arguments[i]) < Math.abs(min)) {
-      min = arguments[i];
-    }
-  }
-  return min;
-}
