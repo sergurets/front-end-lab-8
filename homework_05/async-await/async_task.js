0dec31a3c900bf60dd01a044b3e90df598da1c4d
sergurets <sergurets@gmail.com>
2018-04-01 16:18:59 +0300

    add iterateRange

---
+++ homework_05/async-await/async_task.js
@@ -0,0 +1,15 @@
+﻿
+async function* rangeGen() {
+  for (let i = 1; i <= 15; i++) {
+    yield i;
+  }
+}
+
+const iterateRange = async () => {
+  var res = 0;
+  for await (let i of rangeGen()) {
+    res = res + i;
+  }
+  return res;
+};
+console.log(iterateRange());
