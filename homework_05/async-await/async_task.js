a426bcc13d9df4c7bfa7bf45d73c14d95951e20b
sergurets <sergurets@gmail.com>
2018-04-01 17:19:54 +0300

    add asyncmagic

--- homework_05/async-await/async_task.js
+++ homework_05/async-await/async_task.js
@@ -1,15 +1,46 @@
-﻿
+﻿const waitFewSec = (msec, triggerFail) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (triggerFail) {
+        reject(false);
+        return;
+      }
+
+      resolve(true);
+    }, msec);
+  });
+};
+
+const asyncFn = async () => {
+  const result = await waitFewSec(1000);
+  return result;
+};
+
+const doAsyncMagic = async function() {
+  var res = await Promise.all([
+    asyncFn(),
+    asyncFn(),
+    asyncFn(),
+    asyncFn()
+  ]).then(function(res) {
+    console.log(res)
+  });
+};
+
+doAsyncMagic();
+
 async function* rangeGen() {
   for (let i = 1; i <= 15; i++) {
     yield i;
   }
 }
 
-const iterateRange = async () => {
+const iterateRange = async function() {
   var res = 0;
   for await (let i of rangeGen()) {
     res = res + i;
   }
   return res;
 };
-console.log(iterateRange());
+
+iterateRange();
